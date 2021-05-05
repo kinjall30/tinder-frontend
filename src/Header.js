34577920b5b07aeb95285643784b7acad72f5a88
KINJAL PRAJAPATI <77926892+kinjall30@users.noreply.github.com>
2021-05-05 21:47:48 +0530

    Tinder Front-end

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./Header.css";
+import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton';
+import ForumIcon from '@material-ui/icons/Forum';
+
+function Header() {
+    return (
+        <div className="header">
+            <IconButton>
+                <PersonIcon fontSize="large" className="header__icon" />
+            </IconButton> 
+
+            <h3 className="title">Start Dating</h3>
+            
+            <IconButton>
+                <ForumIcon fontSize="large" className="header__icon" />
+            </IconButton> 
+        </div>
+    )
+}
+
+export default Header
+
